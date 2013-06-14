6a48eb11b61ac556a750f5e2e41ef792c4e7a188
Lars Steen <larste@gmail.com>
2013-06-14 18:19:16 +0200

    Added the application class

---
+++ src/yeller/app/Application.java
@@ -0,0 +1,18 @@
+package yeller.app;
+
+/**
+ *
+ * @author Lars Steen
+ */
+public class Application 
+{
+    public Application()
+    {
+        
+    }
+    
+    public void run()
+    {
+        
+    }
+}
